ee4056a79ded12381ed48c8d5e9dfc90e09d725b
Santiya Georgieva <santiya.georgigeva@gmail.com>
2024-07-04 01:16:52 +0300

    add gitignore file and update the package.jsons files and change the port of the server app

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ mongoose
 // mongoose.set('debug', true);
 
 // START THE SERVER
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
